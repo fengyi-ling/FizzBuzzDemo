c9e7a62036a257132c5a915dc9f7e70b52b46f42
fengyi <fengyi.ling@exxonmobile.com>
2022-10-09 15:54:11 +0800

    [Fengyi] Refactor: extract method to show intent

--- FizzBuzz/FizzBuzz.cs
+++ FizzBuzz/FizzBuzz.cs
@@ -5,22 +5,32 @@ public class FizzBuzz
     public string PlayGame(int givenNumber)
     {
         
-        if(givenNumber % 15 == 0)
+        if(IsMultipleOf5(givenNumber) && IsMultipleOf3(givenNumber))
         {
             return "FizzBuzz";
         }
         
         
-        if(givenNumber % 5 == 0)
+        if(IsMultipleOf5(givenNumber))
         {
             return "Buzz";
         }
         
-        if (givenNumber % 3 == 0)
+        if (IsMultipleOf3(givenNumber))
         {
             return "Fizz";
         }
 
         return givenNumber.ToString();
+    }
+
+    private static bool IsMultipleOf3(int givenNumber)
+    {
+        return givenNumber % 3 == 0;
+    }
+
+    private static bool IsMultipleOf5(int givenNumber)
+    {
+        return givenNumber % 5 == 0;
     }
 }
